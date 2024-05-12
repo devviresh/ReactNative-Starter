21d17dc215d47d0e80e1a6220caae3214e07a38e
devviresh <vireshdev5e@gmail.com>
2024-05-12 21:43:42 +0530

    Counter screen added

--- src/screens/Counter.js
+++ src/screens/Counter.js
@@ -1,9 +1,19 @@
-import React from "react";
-import { FlatList, Text, View, StyleSheet, Button} from "react-native";
+import React, { useState } from "react";
+import { FlatList, Text, View, StyleSheet, Button } from "react-native";
 
-const CounterScreen = ()=>{
-    <View>
+const CounterScreen = () => {
+    const [count, setCount]=useState(0)
+
+    return <View>
         <Text>Counter Screen</Text>
+        <Button title="Increase" onPress={() => {
+            setCount(count+1);
+        }} />
+        <Button title="Decrease" onPress={() => {
+            setCount(count-1);
+        }} />
+        <Text>Current Count : {count}</Text>
+
     </View>
 
 }
